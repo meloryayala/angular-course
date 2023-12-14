aa97a4e9b5b7f573a6a0a7d2b5c1795ce84a65a0
Melory Ayala <melory.ayala@gmail.com>
2023-12-14 12:04:34 +0100

    chore: add todo-list initial project

---
+++ todo-app/src/app/app.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  template: `
+    <router-outlet></router-outlet>
+    <h1>Hello</h1>
+  `,
+
+})
+export class AppComponent {
+
+}
