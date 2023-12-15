c07d1a8d57ac72f54e0c655a1cca8bc3286c3233
Melory Ayala <melory.ayala@gmail.com>
2023-12-15 11:29:04 +0100

    feat: add shared module, add header, list and search components

---
+++ pokedex/src/app/shared/shared.module.ts
@@ -0,0 +1,23 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { HeaderComponent } from './header/header.component';
+import { SearchComponent } from './search/search.component';
+import { ListComponent } from './list/list.component';
+
+
+@NgModule({
+  declarations: [
+    HeaderComponent,
+    SearchComponent,
+    ListComponent
+  ],
+  exports: [
+    HeaderComponent,
+    SearchComponent,
+    ListComponent,
+  ],
+  imports: [
+    CommonModule
+  ]
+})
+export class SharedModule { }
